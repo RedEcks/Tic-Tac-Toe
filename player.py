2de9351aedb4bcaa9dd6022321f29b1d3c312cd3
Xavier Dunkley <xavierxv18@gmail.com>
2023-11-22 15:24:06 -0500

    created class for human and computer

--- player.py
+++ player.py
@@ -5,3 +5,21 @@ class Player:
     def __init__(self, letter):
         #letter is x or o
         self.letter = letter
+        
+    # we want all players to get their next move given a game 
+    def get_move(self, game):
+        pass
+    
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+        
+    def get_move(self, game):
+        return super().get_move(game)
+    
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+        
+    def get_move(self, game):
+        pass
