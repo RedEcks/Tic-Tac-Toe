29979fa39c34ecd8e6842bad8499bfb4512b804d
Xavier Dunkley <xavierxv18@gmail.com>
2023-11-23 03:21:41 -0500

    almost done with player

--- game.py
+++ game.py
@@ -12,4 +12,17 @@ class TicTacToe:
         #0 | 1 | 2 etc (tells us what number corresponds to what box)
         number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
-            print()
+            print('| '+' | '.join(row) + ' |')
+            
+    def available_moves(self):
+        return [i for i, spot in enumerate(self.board) if spot== ' ']
+
+
+def play(game, x_player, o_player, print_game=True):
+    if print_game:
+        game.print_board_nums()
+            
+    letter = 'X' #starting letter
+    #iterate while the game still has empty squares
+    #(we don't have to worry about winner because we'll just return that
+    # which breaks the loop)
